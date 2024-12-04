60ae380b60b22c0559554343c6ac94ecdb0d05aa
Fadhil <11231023@student.itk.ac.id>
2024-12-04 13:58:09 +0800

    Update Workflow, fix font add to build

--- src/font_resources.h
+++ src/font_resources.h
@@ -1,11 +1,6 @@
-
 #pragma once
 #include <string>
 
 inline std::string getFontPath(const char* fontName) {
-#ifdef _WIN32
     return std::string("font/") + fontName;
-#else
-    return std::string("font/") + fontName;
-#endif
 }
